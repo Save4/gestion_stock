9075173bf3c9367fb30972652f359fb4ea320fa4
yves <kabiryves90@gmail.com>
2020-12-01 20:00:25 +0200

    add layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="container">
-   <img src="images/back.jpg" alt="Best managenent" title="Best management">
-    </div>
-</div>
+
 @endsection
