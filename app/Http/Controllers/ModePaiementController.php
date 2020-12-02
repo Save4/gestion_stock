1a11a6c32e31df793c0900c1bfa61101650f7074
yves <kabiryves90@gmail.com>
2020-12-02 15:09:24 +0200

    create controller mode_paiement

---
+++ app/Http/Controllers/ModePaiementController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mode_paiement;
+use Illuminate\Http\Request;
+
+class ModePaiementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Mode_paiement  $mode_paiement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Mode_paiement $mode_paiement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Mode_paiement  $mode_paiement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Mode_paiement $mode_paiement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Mode_paiement  $mode_paiement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Mode_paiement $mode_paiement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Mode_paiement  $mode_paiement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Mode_paiement $mode_paiement)
+    {
+        //
+    }
+}
