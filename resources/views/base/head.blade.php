76a65200ce9418eb580db6b799862af0a9aa2077
yves <kabiryves90@gmail.com>
2020-12-03 21:24:22 +0200

    modify head

--- resources/views/base/head.blade.php
+++ resources/views/base/head.blade.php
@@ -29,5 +29,9 @@
   <!-- Custom Style-->
   <link href="{{ asset('assets/css/app-style.css')}}" rel="stylesheet"/>
 
+   <!--Data Tables -->
+  <link href="{{ asset('assets/plugins/bootstrap-datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{ asset('assets/plugins/bootstrap-datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet" type="text/css">
+
 
 </head>
