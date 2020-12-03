94714c52a3f29a233d38583460bbec6db3a54919
yves <kabiryves90@gmail.com>
2020-12-04 01:58:28 +0200

    modify script

--- resources/views/base/script.blade.php
+++ resources/views/base/script.blade.php
@@ -12,35 +12,7 @@
 <!-- Custom scripts -->
 <script src="{{ asset('assets/js/app-script.js')}}"></script>
 
-<!-- Vector map JavaScript -->
-<script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
-<script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
-<!-- Sparkline JS -->
-<script src="{{ asset('assets/plugins/sparkline-charts/jquery.sparkline.min.js')}}"></script>
-<!-- Chart js -->
-<script src="{{ asset('assets/plugins/Chart.js/Chart.min.js')}}"></script>
-<!--notification js -->
-<script src="{{ asset('assets/plugins/notifications/js/lobibox.min.js')}}"></script>
-<script src="{{ asset('assets/plugins/notifications/js/notifications.min.js')}}"></script>
-<!-- Index js -->
-<script src="{{ asset('assets/js/index2.js')}}"></script>
 
- 
-
-
-     <!-- Bootstrap core JavaScript-->
-  <script src="{{ asset('js/jquery.min.js')}}"></script>
-  <script src="{{ asset('js/popper.min.js')}}"></script>
-  <script src="{{ asset('js/bootstrap.min.js')}}"></script>
-  
-  <!-- simplebar js -->
-  <script src="assets/plugins/simplebar/js/simplebar.js"></script>
-  <!-- waves effect js -->
-  <script src="{{ asset('js/waves.js')}}"></script>
-  <!-- sidebar-menu js -->
-  <script src="{{ asset('js/sidebar-menu.js')}}"></script>
-  <!-- Custom scripts -->
-  <script src="{{ asset('js/app-script.js')}}"></script>
 
   <!--Data Tables js-->
   <script src="{{ asset('js/jquery.dataTables.min.js')}}"></script>
@@ -64,10 +36,24 @@
         lengthChange: false,
         buttons: [ 'copy', 'excel', 'pdf', 'print', 'colvis' ]
       } );
- 
+
      table.buttons().container()
         .appendTo( '#example_wrapper .col-md-6:eq(0)' );
-      
+
       } );
 
     </script>
+
+
+<!-- Vector map JavaScript -->
+<script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
+<script src="{{ asset('assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+<!-- Sparkline JS -->
+<script src="{{ asset('assets/plugins/sparkline-charts/jquery.sparkline.min.js')}}"></script>
+<!-- Chart js -->
+<script src="{{ asset('assets/plugins/Chart.js/Chart.min.js')}}"></script>
+<!--notification js -->
+<script src="{{ asset('assets/plugins/notifications/js/lobibox.min.js')}}"></script>
+<script src="{{ asset('assets/plugins/notifications/js/notifications.min.js')}}"></script>
+<!-- Index js -->
+<script src="{{ asset('assets/js/index2.js')}}"></script>
