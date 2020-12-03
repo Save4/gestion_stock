7068892201737264af8b1efd3d63d2a4b2c53a0a
yves <kabiryves90@gmail.com>
2020-12-03 13:54:02 +0200

    modify script

--- resources/views/base/script.blade.php
+++ resources/views/base/script.blade.php
@@ -24,3 +24,33 @@
 <script src="{{ asset('assets/plugins/notifications/js/notifications.min.js')}}"></script>
 <!-- Index js -->
 <script src="{{ asset('assets/js/index2.js')}}"></script>
+
+  <!--Data Tables js-->
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/jquery.dataTables.min.js')}}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/dataTables.bootstrap4.min.js')}}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/dataTables.buttons.min.js')}}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/buttons.bootstrap4.min.js')}}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/jszip.min.js')}}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/pdfmake.min.js')}}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/vfs_fonts.js')}}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/buttons.html5.min.js')}}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/buttons.print.min.js')}}"></script>
+  <script src="{{ asset('assets/plugins/bootstrap-datatable/js/buttons.colVis.min.js')}}"></script>
+
+    <script>
+     $(document).ready(function() {
+      //Default data table
+       $('#default-datatable').DataTable();
+
+
+       var table = $('#example').DataTable( {
+        lengthChange: false,
+        buttons: [ 'copy', 'excel', 'pdf', 'print', 'colvis' ]
+      } );
+
+     table.buttons().container()
+        .appendTo( '#example_wrapper .col-md-6:eq(0)' );
+
+      } );
+
+    </script>
