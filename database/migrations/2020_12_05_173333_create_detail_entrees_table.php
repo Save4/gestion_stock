d08b80a11140f602c20eb2caa3ec644098daee70
yves <kabiryves90@gmail.com>
2020-12-06 00:20:57 +0200

    add table detail entre

---
+++ database/migrations/2020_12_05_173333_create_detail_entrees_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailEntreesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_entrees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('entree_id');
+            $table->unsignedBigInteger('produit_id');
+            $table->integer('quantite');
+            $table->integer('prix_achat');
+            $table->integer('prix_vente');
+           $table->timestamps();
+           $table->foreign('entree_id')
+                  ->references('id')
+                  ->on('entrees')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+           $table->foreign('produit_id')
+                  ->references('id')
+                  ->on('produits')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_entrees');
+    }
+}
