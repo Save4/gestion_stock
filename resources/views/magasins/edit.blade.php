d4cfe3dcc7e785cc2e7b9e9089e16a8f2400bc77
Isaya M Pombo <isayamlelapombo@gmail.com>
2020-12-07 12:32:54 +0100

    Update: add validation contoller message

--- resources/views/magasins/edit.blade.php
+++ resources/views/magasins/edit.blade.php
@@ -13,23 +13,23 @@
                 </ol>
             </div>
             <!-- <div class="col-sm-3">
-                <div class="btn-group float-sm-right">
-                    <button type="button" class="btn btn-outline-primary waves-effect waves-light"><i
-                            class="fa fa-cog mr-1"></i> Setting</button>
-                    <button type="button"
-                        class="btn btn-outline-primary dropdown-toggle dropdown-toggle-split waves-effect waves-light"
-                        data-toggle="dropdown">
-                        <span class="caret"></span>
-                    </button>
-                    <div class="dropdown-menu">
-                        <a href="javaScript:void();" class="dropdown-item">Action</a>
-                        <a href="javaScript:void();" class="dropdown-item">Another action</a>
-                        <a href="javaScript:void();" class="dropdown-item">Something else here</a>
-                        <div class="dropdown-divider"></div>
-                        <a href="javaScript:void();" class="dropdown-item">Separated link</a>
-                    </div>
-                </div>
-            </div> -->
+                        <div class="btn-group float-sm-right">
+                            <button type="button" class="btn btn-outline-primary waves-effect waves-light"><i
+                                    class="fa fa-cog mr-1"></i> Setting</button>
+                            <button type="button"
+                                class="btn btn-outline-primary dropdown-toggle dropdown-toggle-split waves-effect waves-light"
+                                data-toggle="dropdown">
+                                <span class="caret"></span>
+                            </button>
+                            <div class="dropdown-menu">
+                                <a href="javaScript:void();" class="dropdown-item">Action</a>
+                                <a href="javaScript:void();" class="dropdown-item">Another action</a>
+                                <a href="javaScript:void();" class="dropdown-item">Something else here</a>
+                                <div class="dropdown-divider"></div>
+                                <a href="javaScript:void();" class="dropdown-item">Separated link</a>
+                            </div>
+                        </div>
+                    </div> -->
         </div>
         <!-- End Breadcrumb-->
         <div class="row">
@@ -48,14 +48,19 @@
                                 <div class="col-sm-10">
                                     <input type="text" name="nom_magasin" value="{{ $magasin->nom_magasin }}"
                                         class="form-control" id="input-1" required>
+
+                                    @error('nom_magasin')
+                                        <code> {{ $message }}</code>
+                                    @enderror
                                 </div>
                             </div>
 
 
 
                             <div class="form-footer">
-                                <button type="submit" class="btn btn-primary shadow-primary m-1"><i class="fa fa-times"></i>
-                                    Annuler</button>
+                                <a href="{{ route('magasins.index') }}"><button type="button"
+                                        class="btn btn-primary shadow-primary m-1"><i class="fa fa-times"></i>
+                                        Annuler</button></a>
                                 <button type="reset" class="btn btn-dark shadow-dark m-1"><i class="fa fa-times"></i>
                                     Reinitialiser</button>
                                 <button type="submit" class="btn btn-success shadow-success m-1"><i
