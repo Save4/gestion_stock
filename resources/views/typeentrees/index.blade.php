2b8e81fbd6a6eff51f83175b265c5083c04dc6ea
somali <oliviersomali@gmail.com>
2020-12-07 08:18:56 +0200

    Update index

--- resources/views/typeentrees/index.blade.php
+++ resources/views/typeentrees/index.blade.php
@@ -5,13 +5,17 @@
     <!-- Breadcrumb-->
     <div class="row pt-2 pb-2">
         <div class="col-sm-9">
-            <h4 class="page-title">Les types des entrées</h4>
-
+            <h4 class="page-title">Table Types des Entrées</h4>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{url('/home')}}">Home</a></li>
+                <!-- <li class="breadcrumb-item"><a href="{{url('typeentrees')}}">Fournisseurs</a></li> -->
+                <li class="breadcrumb-item active" aria-current="page">Table Type Entrees</li>
+            </ol>
         </div>
         <div class="col-sm-3">
             <div class="btn-group float-sm-right">
-                <button class="btn btn-outline-primary waves-effect waves-light" data-toggle="modal"
-                    data-target="#smallsizemodal"><i class="fa fa-cog mr-1"></i> Ajouter</button>
+                <button class="btn btn-primary mr-1" data-toggle="modal"
+                    data-target="#smallsizemodal"><i class="fa fa-plus"></i> Ajouter Type Entrees</button>
 
             </div>
         </div>
@@ -47,7 +51,7 @@
                                                       class="d-inline">
                                                       @csrf
                                                       <button type="submit"
-                                                          onclick="return confirm('Voulez vous supprimer le type d'entre ?')"
+                                                          onclick="return confirm('Voulez vous supprimer le type d\'entre ?')"
                                                           class="btn btn-danger btn-sm" title="Delete">
                                                           <span class="fa fa-trash"></span></button>
                                                       @method('DELETE')
@@ -96,7 +100,7 @@
                                     </div>
 
 
-                                    <button type="submit" class="btn btn-primary"> Enregistrer</button>
+                                    <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Enregistrer</button>
                                 </form>
                             </div>
                         </div>
