e8b36783364639611d0c706b850b752056416fad
somali <oliviersomali@gmail.com>
2020-12-07 15:21:28 +0200

    Update edit.blade.php : add a select value of categorie

--- resources/views/produits/edit.blade.php
+++ resources/views/produits/edit.blade.php
@@ -51,6 +51,17 @@
                             </div>
                         </div>
                         <div class="form-group row">
+                        <label for="input-1" class="col-sm-2 col-form-label"> Categorie</label>
+                            <div class="col-sm-10">
+                                <select class="form-control" name="categorie_id" id="categorie_id">
+                                    @foreach($categories as $categorie)
+                                    <option value="{{$categorie->id}}"{!! $produit->categorie_id==$categorie->id ? 'selected="selected"':'' !!}>
+                                        {{ $categorie->nom_categorie}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group row">
                         <label for="input-1" class="col-sm-2 col-form-label"> Unite Mesure</label>
                             <div class="col-sm-10">
                                 <select class="form-control" name="unitemesure_id" id="unitemesure_id">
