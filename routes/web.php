9599b298b9e80fefb3f777f43b41943315591458
yves <kabiryves90@gmail.com>
2020-12-08 20:19:56 +0200

    update route

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,6 @@ Route::resource('magasins', 'MagasinController');
 Route::resource('fournisseurs','FournisseursController');
 
 Route::resource('entrees','EntreesController');
-Route::get('detail_entrees/index/{Entree}','EntreesController@detail');
 
 
 
