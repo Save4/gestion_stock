9658ce8318d18df5ecccde1542d72304e8b84a03
yves <kabiryves90@gmail.com>
2020-12-08 09:12:41 +0200

    update controller entree

--- app/Http/Controllers/EntreesController.php
+++ app/Http/Controllers/EntreesController.php
@@ -138,17 +138,16 @@ class EntreesController extends Controller
     public function detail(Entree $entree)
     {
 
-        $fournisseurs = Fournisseur::all();
-        $magasins = Magasin::all();
-        $typeentrees = Typeentree::all();
-        $mode_paiements = Mode_paiement::all();
-
-        $entree = Entree::find($entree->id);
+      $entrees = DB::table('entrees')
+          ->join('fournisseurs', 'entrees.fournisseur_id', 'fournisseurs.id')
+          ->join('magasins', 'entrees.magasin_id', 'magasins.id')
+          ->join('typeentrees', 'entrees.type_entree_id', 'typeentrees.id')
+          ->join('mode_paiements', 'entrees.mode_paiement_id', 'mode_paiements.id')
+          ->where('entrees.id', $entree->id)
+          ->select('fournisseurs.*', 'magasins.*', 'typeentrees.*', 'mode_paiements.*', 'entrees.*')
+          ->get();
+          
         return view('entrees.edit', [
-            'fournisseurs' => $fournisseurs,
-            'magasins' => $magasins,
-            'typeentrees' => $typeentrees,
-            'mode_paiements' => $mode_paiements,
             'entree' => $entree
         ]);
     }
