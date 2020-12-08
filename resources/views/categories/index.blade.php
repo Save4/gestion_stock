3ea6dbe9cb044423cf607ca865e76541c25b618a
somali <oliviersomali@gmail.com>
2020-12-08 11:56:29 +0200

    Update categories/index view  : add error message validation

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -92,6 +92,17 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header"><i class="fa fa-table"></i>Liste des Categories</div>
+                @error('nom_magasin')
+                    <div class="alert alert-light-danger alert-dismissible" role="alert">
+                        <button type="button" class="close" data-dismiss="alert">×</button>
+                        <div class="alert-icon">
+                            <i class="icon-close"></i>
+                        </div>
+                        <div class="alert-message">
+                            <span> {{ $message }}</span>
+                        </div>
+                    </div>
+                @enderror
                 <div class="card-body">
                     <div class="table-responsive">
                         <div class="dataTables_wrapper container-fluid dt-bootstrap4" id="example_wrapper">
