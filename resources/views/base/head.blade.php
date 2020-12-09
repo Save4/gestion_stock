d298cea8af1621772793d12c33aab15e3afead78
yves <kabiryves90@gmail.com>
2020-12-09 11:58:25 +0200

    update title head

--- resources/views/base/head.blade.php
+++ resources/views/base/head.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
   <meta name="description" content=""/>
   <meta name="author" content=""/>
-  <title>@yield(config('app.name'))</title>
+  <title>@yield('title',config('app.name'))</title>
   <!--favicon-->
   <link rel="icon" href="{{ asset('assets/images/favicon.ico')}}" type="image/x-icon">
   <!-- notifications css -->
