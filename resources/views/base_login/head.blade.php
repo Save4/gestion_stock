41eb6b0477f978d48e781c1f9c065a4aaeafd3ea
yves <kabiryves90@gmail.com>
2020-12-11 09:34:52 +0200

    move link css and js for login in folder base_login

---
+++ resources/views/base_login/head.blade.php
@@ -0,0 +1,11 @@
+<!-- ================== BEGIN BASE CSS STYLE ================== -->
+  <link href="{{ asset('detail/css/jquery-ui/themes/base/minified/jquery-ui.min.css')}}" rel="stylesheet" />
+  <link href="{{ asset('detail/css/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" />
+  <link href="{{ asset('detail/css/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" />
+  <link href="{{ asset('detail/css/ionicons/css/ionicons.min.css')}}" rel="stylesheet" />
+  <link href="{{ asset('detail/css/animate.min.css')}}" rel="stylesheet" />
+  <link href="{{ asset('detail/css/style.min.css')}}" rel="stylesheet" />
+  <link href="{{ asset('detail/css/style-responsive.min.css')}}" rel="stylesheet" />
+  <link href="{{ asset('detail/css/theme/default.css')}}" rel="stylesheet" id="theme" />
+  <!-- ================== END BASE CSS STYLE ================== -->
+  <link rel="stylesheet" href="{{ asset('detail/css/bootstrap-select.css')}}">
