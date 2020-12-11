e32702a06b1fca358bd6fdac613996e96beb32ff
somali <oliviersomali@gmail.com>
2020-12-11 11:00:31 +0200

    update home for design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,29 +17,29 @@
     <div class="row">
 
 
-        <div class="col-md-4">
+        <div class="col-md-3">
           <a href="{{route('produits.index')}}" class="thumbnail">
             <img src="images/produit.jpg" alt="...">
           </a>  <h4 class="text-center">Les produits</h4>
         </div>
-<div class="col-md-4">
+<div class="col-md-3">
   <a href="{{route('entrees.index')}}" class="thumbnail">
     <img src="images/entre.jpg" class="img-responsive" alt="...">
   </a>  <h4 class="text-center">Les entrees</h4>
 </div>
-<div class="col-md-4">
+<!-- <div class="col-md-4">
   <a href="{{route('detail_entrees.index')}}" class="thumbnail">
     <img src="images/detail.jpg" alt="...">
   </a>  <h4 class="text-center">Le detail des entrees</h4>
-</div>
+</div> -->
 
-<div class="col-md-4">
+<div class="col-md-3">
   <a href="{{route('magasins.index')}}" class="thumbnail">
     <img src="images/magasin.jpg" class="img-responsive" alt="...">
   </a>  <h4 class="text-center">les magasins</h4>
 </div>
 
-<div class="col-md-4">
+<div class="col-md-3">
   <a href="{{route('fournisseurs.index')}}" class="thumbnail">
     <img src="images/fournisseur.jpg" class="img-responsive" alt="...">
   </a>  <h4 class="text-center">Les fournisseurs</h4>
